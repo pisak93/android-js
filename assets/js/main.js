fc47d92e011c0509adcb657d2daeb1b261c0f432
pisak93 <sebastian.marquez93@hotmail.com>
2024-11-08 09:49:23 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,118 @@
+document.addEventListener("DOMContentLoaded",function(e){
+    updateUsers();
+imprimirNombres();
+
+document.querySelector("#formulario").addEventListener("submit", function(e){
+    e.preventDefault();
+    const usuario =document.querySelector("#usuario").value;
+  
+    if(usuario==""){
+        return false;
+    }
+    else{
+        fetch("https://app-mongo-2.onrender.com/add",
+            {
+                method:"POST",
+                headers:{"Content-type":"application/json"},
+                body: JSON.stringify({usuario:usuario})
+            }
+        )
+        .then(function(res){
+            return res.json();
+        })
+        .then( function(data){
+            if(data.response=="success"){
+                location.reload();
+            }
+        })
+    }
+    });
+    });
+    
+
+    var datosBD;
+    
+async function updateUsers(){
+
+
+ var bd= await fetch("https://app-mongo-2.onrender.com/getall")
+    .then(function(res){
+         return res.json()
+         })
+    .then(function(datos){
+
+        if(datos.response =="success"){
+            const usuarios=datos.data;
+            datosBD=usuarios;
+
+    var elementosUsuarios="";
+
+            usuarios.forEach(function(usuario){
+                console.log(usuario.nombre);
+                
+                elementosUsuarios+="<div class='usuario' id='"+usuario._id+"'>"+usuario.nombre+"</div>";
+                document.querySelector(".lista-usuarios").innerHTML = elementosUsuarios;
+            })
+        }
+    });
+
+
+imprimirNombres();
+
+
+    }
+
+    function imprimirNombres(){
+        console.log(datosBD);
+    }
+
+
+   
+  /*document.querySelector("#formulario").addEventListener("submit", function(e){
+    e.preventDefault();
+    const usuario =document.querySelector("#usuario").value;
+    const correo =document.querySelector("#correo").value;
+    if(usuario=="" && correo==""){
+        return false;
+    }
+    else{
+        fetch("http://localhost:5000/add",
+            {
+                method:"POST",
+                headers:{"Content-type":"application/json"},
+                body: JSON.stringify({usuario:usuario, correo:correo})
+            }
+        )
+        .then(function(res){
+            return res.json();
+        })
+        .then( function(data){
+            if(data.response=="success"){
+                updateUsers();
+            }
+        })
+    }
+    });
+ function traerPuntos(){
+
+    }
+
+    fetch("http://localhost:5000/add",
+        {
+            method:"POST",
+            headers:{"Content-type":"application/json"},
+            body: JSON.stringify({usuario:usuario, correo:correo})
+        }
+    )
+    .then(function(res){
+        return res.json();
+    })
+    .then( function(data){
+        if(data.response=="success"){
+            updateUsers();
+        }
+    });
+ 
+  */  
+
+  
